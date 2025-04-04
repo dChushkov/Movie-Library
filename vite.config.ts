5d72959011d53081f60160e20f47213a61fedf50
Dimitar Chushkov <chushkov90@gmail.com>
2025-04-05 00:54:09 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Movie-Library/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
